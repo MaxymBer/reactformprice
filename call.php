07f657210fbd203f1741ced1992b328e222ee94a
Limitrof <alexinby@gmail.com>
2016-11-09 13:33:51 +0200

    checkbox hide for row without price

--- call.php
+++ call.php
@@ -38,6 +38,11 @@ background-color:white;
 	padding-top:15px;
 	border-bottom:1px solid #b6b6bb;
 }
+.checkboxforprice{
+display:block;
+margin-right:20px !important;
+float:left;
+}
 </style>
 </head>
 <body>
@@ -81,13 +86,17 @@ class ProductSelect extends React.Component {
 } 
 
  
-  render() {
-	  var arrOfOptions =[];
- this.props.category.map(function(currOption,index){
-	 arrOfOptions.push(<option key={index}  value={currOption.valOf}  >{currOption.strInfo}</option>);
-	});	  
-    return <div className=""><select  onChange={this.props.onClick}  value={this.state.value}>{arrOfOptions}</select></div>;
-  }
+ render() {
+/* 	 if (this.props.category=='input') {
+		 return <div><input type="text"  onChange={this.props.onClick}  value={this.state.value}/></div>;
+	 } else { */
+		 var arrOfOptions =[];
+		 this.props.category.map(function(currOption,index){
+		 arrOfOptions.push(<option key={index}  value={currOption.valOf}  >{currOption.strInfo}</option>);
+		});
+		return <div><select onChange={this.props.onClick}  value={this.state.value}>{arrOfOptions}</select></div>;
+	 /* } */
+ }
 /*   
  handleClickSelect(){
 	 this.setState({ value:  e.target.value });
@@ -107,13 +116,13 @@ class DankButton extends React.Component {
 	this.state = {
 		allAmount: 0,
 		resultPrices:[  
-			  {indexid:10,category: 'Web-разработка', baseprice: '449.99', price: '0', stocked: true, name: 'Сайт', formula: 'multi',multinumber:1, needit: false},
-			  {indexid:11,category: 'Web-разработка',  baseprice: '1.99',price: '0', stocked: false, name: 'IT консалтинг', formula: 'multi',multinumber:1, needit: false},
+			  {indexid:10,category: 'Вводные данные', baseprice: '0', price: '0', stocked: true, name: 'Укажите длительность Программы', formula: 'multi',multinumber:1, needit: false},
+			  {indexid:11,category: 'Вводные данные',  baseprice: '0', price: '0', stocked: false, name: 'IT консалтинг', formula: 'multi',multinumber:1, needit: false},
 			  {indexid:12,category: 'Печатная продукция',  baseprice: '29.99',price: '0', stocked: true, name: 'Печать буклета А5', formula: 'multi',multinumber:1, needit: false},
 			  {indexid:13,category: 'Печатная продукция', baseprice: '99.99', price: '0', stocked: true, name: 'Печать на пакете', formula: 'multi',multinumber:1, needit: false},
 			  {indexid:14,category: 'Дизайн',  baseprice: '9999.99',price: '0', stocked: false, name: 'Ночь с дизайнером', formula: 'multi',multinumber:1, needit: false},
 			  {indexid:15,category: 'Дизайн',  baseprice: '77.99',price: '0', stocked: true, name: 'Дизайн буклета', formula: 'multi',multinumber:1, needit: false},
-			  {indexid:16,category: 'Дизайн', baseprice: '3.99', price: '0', stocked: true, name: 'Баннер', formula: 'multi',multinumber:1, needit: false}	
+			  {indexid:16,category: 'Призовой фонд', baseprice: '3.99', price: '0', stocked: true, name: 'Поощрения сотрудников дистрибьюторов (супервайзеров)', formula: 'multi',multinumber:1, needit: false}	
 			  ]
 	}
   } 
@@ -121,13 +130,25 @@ class DankButton extends React.Component {
 	// render
 	render(content) { 
 		var PRODUCTS = [
-	  {indexid:10,category: 'Web-разработка', showwordprefix:'от', comment:'some comment text', price: '449.99', stocked: true, name: 'Сайт', arrOfVlue:[{valOf:1,strInfo:'Сайт-визитка'},{valOf:2,strInfo:'Лендинговая страница'},{valOf:5,strInfo:'Портал'}]},
-	  {indexid:11,category: 'Web-разработка', showwordprefix:'от', comment:'some comment text', price: '1.99', stocked: false, name: 'IT консалтинг', arrOfVlue:[{valOf:1,strInfo:'Умный совет'}, {valOf:10,strInfo:'Выслушать Ваши рассуждения с умным видом'}]},
-	  {indexid:12,category: 'Печатная продукция', showwordprefix:'от', comment:'some comment text', price: '29.99', stocked: true, name: 'Печать буклета А5', arrOfVlue:[{valOf:1,strInfo:'Тираж до 100'},{valOf:0.9,strInfo:'Тираж более 1000'}]},
-	  {indexid:13,category: 'Печатная продукция', showwordprefix:'от', comment:'some comment text', price: '99.99', stocked: true, name: 'Печать на пакете', arrOfVlue:[{valOf:'888',strInfo:'46'}]},
-	  {indexid:14,category: 'Дизайн', showwordprefix:'от', comment:'some comment text', price: '9999.99', stocked: false, name: 'Ночь с дизайнером', arrOfVlue:[{valOf:'888',strInfo:'46'}]},
-	  {indexid:15,category: 'Дизайн', showwordprefix:'от', comment:'some comment text', price: '77.99', stocked: true, name: 'Дизайн буклета', arrOfVlue:[{valOf:'888',strInfo:'46'}]},
-	  {indexid:16,category: 'Дизайн', showwordprefix:'от', comment:'some comment text', price: '3.99', stocked: true, name: 'Баннер', arrOfVlue:[{valOf:'888',strInfo:'46'}]}
+			{
+				indexid:10,
+				usecheckbox:false,
+				category: 'Web-разработка', 
+				showwordprefix:'', 
+				comment:'no comments', 
+				price: '', 
+				stocked: true, 
+				name: 'Укажите длительность Программы', 
+				arrOfVlue:[{valOf:1,strInfo:'1-3 месяца'},{valOf:2,strInfo:'4-6 месяцев'},{valOf:5,strInfo:'более 6 месяцев'}]
+			},
+	  
+	  {indexid:11,usecheckbox:true,category: 'Web-разработка', showwordprefix:'от', comment:'some comment text', price: '1.99', stocked: false, name: 'Укажите кол-во дистрибъюторов', arrOfVlue:'inputdistributor'},
+	  
+	  {indexid:12,usecheckbox:true,category: 'Печатная продукция', showwordprefix:'от', comment:'some comment text', price: '29.99', stocked: true, name: 'Печать буклета А5', arrOfVlue:[{valOf:1,strInfo:'Тираж до 100'},{valOf:0.9,strInfo:'Тираж более 1000'}]},
+	  {indexid:13,usecheckbox:true,category: 'Печатная продукция', showwordprefix:'от', comment:'some comment text', price: '99.99', stocked: true, name: 'Печать на пакете', arrOfVlue:[{valOf:'888',strInfo:'46'}]},
+	  {indexid:14,usecheckbox:true,category: 'Дизайн', showwordprefix:'от', comment:'some comment text', price: '9999.99', stocked: false, name: 'Ночь с дизайнером', arrOfVlue:[{valOf:'888',strInfo:'46'}]},
+	  {indexid:15,usecheckbox:true,category: 'Дизайн', showwordprefix:'от', comment:'some comment text', price: '77.99', stocked: true, name: 'Дизайн буклета', arrOfVlue:[{valOf:'888',strInfo:'46'}]},
+	  {indexid:16,usecheckbox:true,category: 'Призовой фонд', showwordprefix:'от', comment:'some comment text', price: '3.99', stocked: true, name: 'Поощрения сотрудников дистрибьюторов (супервайзеров)', arrOfVlue:[{valOf:'888',strInfo:'46'}]}
 		];
 		var buttonListElements = [];
 		var lastCategory = null;
@@ -136,6 +157,19 @@ class DankButton extends React.Component {
 			////////////////
 			// child block
 			PRODUCTS.map(function(product,index) {
+				//UNIC FORMULA
+				var usersControlFormula = [];
+				if (product.arrOfVlue=='inputdistributor') {
+					usersControlFormula.push(<div><input type="text"  onChange={this.setDistributorQuantiy.bind(this,index)}  value={this.state.value}/></div>);
+				} else {		
+					usersControlFormula.push(<ProductSelect onClick={this.changeItemPriceMulti.bind(this,index)} category={product.arrOfVlue} key={product.indexid} />);
+				}
+				
+				//USE OR NOT checkbox
+				var useOrNotCheckbox = []
+				if (product.usecheckbox) {
+					useOrNotCheckbox
+				}
 				  if (product.category !== lastCategory) {
 					buttonListElements.push(<div className="row bcwhite"><div className="col-md-12"><h3>{product.category}</h3></div></div>);
 				 }
@@ -145,13 +179,16 @@ class DankButton extends React.Component {
 				} else {
 					pricewithprefix = product.price;
 				}
+				
 				  buttonListElements.push(
 				  <div  key={product.indexid} className="row" title={product.comment}>
 					<div className="col-md-5 bcgray">
-						<input type='checkbox' id={product.indexid}  onClick={this.handleClickCheckbox.bind(null, product.price,index)} value={product.price} />
+						<input className='checkboxforprice' type='checkbox' id={product.indexid}  onClick={this.handleClickCheckbox.bind(null, product.price,index)} value={product.price} />
 						<label   htmlFor={product.indexid}>{product.name}</label>
 					</div>
-					<div className="col-md-5 bcgray"><ProductSelect onClick={this.changeItemPriceMulti.bind(this,index)} category={product.arrOfVlue} key={product.indexid} /></div>	
+					
+					<div className="col-md-5 bcgray">{usersControlFormula}</div>
+					
 					<div className="col-md-2 bcgray"><SomeSum priceinrow={pricewithprefix} key={product.price} /></div>
 
 				</div>);
@@ -170,6 +207,12 @@ class DankButton extends React.Component {
 	}
 	// render  
 	///////////
+	
+	setDistributorQuantiy(indexinarray,event){
+		  var valueForDistributorPrice = event.target.value;
+		  var newArrForFormula = this.state.resultPrices;
+		  
+	}
 		/***
 		set new value for multinumber in state array (resultPrices) by indexinarray
 		arg1 indexinarray - element index
