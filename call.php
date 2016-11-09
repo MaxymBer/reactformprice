bd1c56d1c02f12f4f446104d4a3215f9391a84f2
Limitrof <alexinby@gmail.com>
2016-11-09 18:43:45 +0200

    two of aight

--- call.php
+++ call.php
@@ -21,8 +21,8 @@
 	width:300px;
 	height:500px;
 	border:1px solid gray;
-	/* position:fixed; */
-position:absolute;
+	position:fixed;
+/* position:absolute; */
 	right:40px;
 	top:50px;
 	border-radius:20px;
@@ -42,6 +42,12 @@ background-color:white;
 display:block;
 margin-right:20px !important;
 float:left;
+}
+.hiddencheckbox{
+display:none;
+}
+.width20{
+width: 20px;
 }
 </style>
 </head>
@@ -116,39 +122,75 @@ class DankButton extends React.Component {
 	this.state = {
 		allAmount: 0,
 		resultPrices:[  
-			  {indexid:10,category: 'Вводные данные', baseprice: '0', price: '0', stocked: true, name: 'Укажите длительность Программы', formula: 'multi',multinumber:1, needit: false},
-			  {indexid:11,category: 'Вводные данные',  baseprice: '0', price: '0', stocked: false, name: 'IT консалтинг', formula: 'multi',multinumber:1, needit: false},
-			  {indexid:12,category: 'Печатная продукция',  baseprice: '29.99',price: '0', stocked: true, name: 'Печать буклета А5', formula: 'multi',multinumber:1, needit: false},
-			  {indexid:13,category: 'Печатная продукция', baseprice: '99.99', price: '0', stocked: true, name: 'Печать на пакете', formula: 'multi',multinumber:1, needit: false},
-			  {indexid:14,category: 'Дизайн',  baseprice: '9999.99',price: '0', stocked: false, name: 'Ночь с дизайнером', formula: 'multi',multinumber:1, needit: false},
-			  {indexid:15,category: 'Дизайн',  baseprice: '77.99',price: '0', stocked: true, name: 'Дизайн буклета', formula: 'multi',multinumber:1, needit: false},
-			  {indexid:16,category: 'Призовой фонд', baseprice: '3.99', price: '0', stocked: true, name: 'Поощрения сотрудников дистрибьюторов (супервайзеров)', formula: 'multi',multinumber:1, needit: false}	
+			  {unicname:'period', indexid:1,category: 'Вводные данные', baseprice: '0', price: '0', stocked: true, name: 'Укажите длительность Программы', formula: 'multi',multinumber:1, needit: false},
+			  {unicname:'disributors_quantity',indexid:2,category: 'Вводные данные',  baseprice: '0', price: '0', stocked: false, name: 'IT консалтинг', formula: 'multi',multinumber:1, needit: false},
+			  {unicname:'region',indexid:3,category: 'Вводные данные',  baseprice: '0',price: '0', stocked: true, name: 'География программы', formula: 'multi',multinumber:1, needit: false},
+			  
+			  {unicname:'disr',indexid:10,category: 'Разработка программы: услуги агентства', baseprice: '550', price: '0', stocked: true, name: 'условия программые', formula: 'multi',multinumber:1, needit: false},
+			  {unicname:'disr',indexid:11,category: 'Разработка программы: услуги агентства', baseprice: '200', price: '0', stocked: true, name: 'концепция со слоганом, название', formula: 'multi',multinumber:1, needit: false},
+			  {unicname:'disr',indexid:12,category: 'Разработка программы: услуги агентства', baseprice: '150', price: '0', stocked: true, name: 'презентация для дистрибьюторов', formula: 'multi',multinumber:1, needit: false},
+			  {unicname:'disr',indexid:13,category: 'Разработка программы: услуги агентства', baseprice: '100', price: '0', stocked: true, name: 'инструкция для дистрибьюторов', formula: 'multi',multinumber:1, needit: false},
+			  
+			  {unicname:'disr',indexid:21,category: 'Платформа для Программы',  baseprice: '7800',price: '0', stocked: false, name: 'Платформа для Программы (website)', formula: 'multi',multinumber:1, needit: false},
+			  {unicname:'disr',indexid:22,category: 'Платформа для Программы',  baseprice: '0',price: '0', stocked: false, name: 'Функционал регистрации участников', formula: 'multi',multinumber:1, needit: false},
+			  {unicname:'disr',indexid:23,category: 'Платформа для Программы',  baseprice: '0',price: '0', stocked: false, name: 'Функционал загрузки оборотов', formula: 'multi',multinumber:1, needit: false},
+			  {unicname:'disr',indexid:24,category: 'Платформа для Программы',  baseprice: '0',price: '0', stocked: false, name: 'Нотификация участников о результатах (e-mail)', formula: 'multi',multinumber:1, needit: false},
+			  {unicname:'disr',indexid:25,category: 'Платформа для Программы',  baseprice: '0',price: '0', stocked: false, name: 'Платформа для Программы (website)', formula: 'multi',multinumber:1, needit: false},
+			  
+			  {unicname:'disr',indexid:31,category: 'Дизайн key visual: информация о Key visual услуга',  baseprice: '1500',price: '0', stocked: true, name: 'Дизайн для платформы (website)', formula: 'multi',multinumber:1, needit: false},
+			  {unicname:'disr',indexid:32,category: 'Дизайн key visual: информация о Key visual услуга',  baseprice: '150',price: '0', stocked: true, name: 'Дизайн рекламного макета Программы', formula: 'multi',multinumber:1, needit: false},			  {unicname:'price_for_banner',indexid:33,category: 'Дизайн key visual: информация о Key visual услуга',  baseprice: '300',price: '1800', stocked: true, name: 'Дизайн баннеров для дистрибьюторов', formula: 'multi',multinumber:5, needit: false},
+			  {unicname:'count_for_banner',indexid:34,category: 'Дизайн key visual: информация о Key visual услуга',  baseprice: '0',price: '0', stocked: true, name: 'Количество баннеров', formula: 'multi',multinumber:1, needit: false},
+			  {unicname:'disr',indexid:35,category: 'Дизайн key visual: информация о Key visual услуга',  baseprice: '150',price: '0', stocked: true, name: 'Дизайн рекламного макета Программы', formula: 'multi',multinumber:1, needit: false},
+			  {unicname:'disr',indexid:36,category: 'Дизайн key visual: информация о Key visual услуга',  baseprice: '200',price: '0', stocked: true, name: 'Дизайн сертификатов победителям (до 3 макетов)', formula: 'multi',multinumber:1, needit: false},
+			  
+			  {unicname:'bonus_for_disr',indexid:16,category: 'Призовой фонд', baseprice: '300', price: '0', stocked: true, name: 'Поощрения сотрудников дистрибьюторов (супервайзеров)', formula: 'multi',multinumber:1, needit: false}	
 			  ]
 	}
   } 
 	///////////
 	// render
 	render(content) { 
+		//////////////
+		// get it form db
+		// Первый раздел - вводные данные
 		var PRODUCTS = [
 			{
-				indexid:10,
-				usecheckbox:false,
-				category: 'Web-разработка', 
+				unicname:'period',
+				indexid:1,
+				usehiddencheckbox:true,
+				category: 'Вводные данные: общая информация о программе лояльности', 
 				showwordprefix:'', 
-				comment:'no comments', 
+				comment:'ориентировочное время проведения Программы', 
 				price: '', 
 				stocked: true, 
 				name: 'Укажите длительность Программы', 
 				arrOfVlue:[{valOf:1,strInfo:'1-3 месяца'},{valOf:2,strInfo:'4-6 месяцев'},{valOf:5,strInfo:'более 6 месяцев'}]
 			},
+	  {unicname:'disributors_quantity',indexid:2,usehiddencheckbox:true,category: 'Вводные данные: общая информация о программе лояльности', showwordprefix:'', comment:'кол-во дистрибьюторов', price: '', stocked: false, name: 'Укажите количество дистрибъюторов', arrOfVlue:'inputdistributor'},
+	  {unicname:'region',indexid:3,usehiddencheckbox:true,category: 'Вводные данные: общая информация о программе лояльности', showwordprefix:'', comment:'some comment text', price: '', stocked: true, name: 'География программы', arrOfVlue:[{valOf:1,strInfo:'Беларусь'},{valOf:2,strInfo:'Россия'},{valOf:2,strInfo:'Украина'},{valOf:3,strInfo:'Казахстан'},{valOf:4,strInfo:'Другие страны'},{valOf:4,strInfo:'Три страны Прим. Украина, Россия, Казахстан'}]},
 	  
-	  {indexid:11,usecheckbox:true,category: 'Web-разработка', showwordprefix:'от', comment:'some comment text', price: '1.99', stocked: false, name: 'Укажите кол-во дистрибъюторов', arrOfVlue:'inputdistributor'},
-	  
-	  {indexid:12,usecheckbox:true,category: 'Печатная продукция', showwordprefix:'от', comment:'some comment text', price: '29.99', stocked: true, name: 'Печать буклета А5', arrOfVlue:[{valOf:1,strInfo:'Тираж до 100'},{valOf:0.9,strInfo:'Тираж более 1000'}]},
-	  {indexid:13,usecheckbox:true,category: 'Печатная продукция', showwordprefix:'от', comment:'some comment text', price: '99.99', stocked: true, name: 'Печать на пакете', arrOfVlue:[{valOf:'888',strInfo:'46'}]},
-	  {indexid:14,usecheckbox:true,category: 'Дизайн', showwordprefix:'от', comment:'some comment text', price: '9999.99', stocked: false, name: 'Ночь с дизайнером', arrOfVlue:[{valOf:'888',strInfo:'46'}]},
-	  {indexid:15,usecheckbox:true,category: 'Дизайн', showwordprefix:'от', comment:'some comment text', price: '77.99', stocked: true, name: 'Дизайн буклета', arrOfVlue:[{valOf:'888',strInfo:'46'}]},
-	  {indexid:16,usecheckbox:true,category: 'Призовой фонд', showwordprefix:'от', comment:'some comment text', price: '3.99', stocked: true, name: 'Поощрения сотрудников дистрибьюторов (супервайзеров)', arrOfVlue:[{valOf:'888',strInfo:'46'}]}
+	  //ВТОРОЙ раздел - разработка программы
+	  {unicname:'rooles',indexid:10,usehiddencheckbox:false,category: 'Разработка программы: услуги агентства', showwordprefix:'от', comment:'условия программы', price: '550', stocked: true, name: 'Условия программы', arrOfVlue:'none'},
+	  {unicname:'disr',indexid:11,usehiddencheckbox:false,category: 'Разработка программы: услуги агентства', showwordprefix:'от', comment:'концепция со слоганом, название', price: '200', stocked: true, name: 'Концепция со слоганом, название', arrOfVlue:'none'},
+	  {unicname:'disr',indexid:12,usehiddencheckbox:false,category: 'Разработка программы: услуги агентства', showwordprefix:'', comment:'презентация для дистрибьюторов', price: '150', stocked: true, name: 'Презентация для дистрибьюторов', arrOfVlue:'none'},
+	  {unicname:'disr',indexid:13,usehiddencheckbox:false,category: 'Разработка программы: услуги агентства', showwordprefix:'', comment:'инструкция для дистрибьюторов', price: '100', stocked: true, name: 'Инструкция для дистрибьюторов', arrOfVlue:'none'},
+	 
+	//ТРЕТИЙ раздел 
+	 {unicname:'disr',indexid:21,usehiddencheckbox:false,category: 'Платформа для Программы лояльности:', showwordprefix:'от', comment:'функционал регистрации участников, функционал загрузки оборотов, нотификация участников о результатах (e-mail), техническая поддержка', price: '7800', stocked: false, name: 'Платформа для Программы (website)', arrOfVlue:'none'},	 
+	 {unicname:'disr',indexid:22,usehiddencheckbox:true,category: 'Платформа для Программы лояльности:', showwordprefix:'', comment:'', price: '', stocked: false, name: ' - функционал регистрации участников', arrOfVlue:'none'},
+	 {unicname:'disr',indexid:23,usehiddencheckbox:true,category: 'Платформа для Программы лояльности:', showwordprefix:'', comment:'', price: '', stocked: false, name: ' - функционал загрузки оборотов', arrOfVlue:'none'},
+	 {unicname:'disr',indexid:24,usehiddencheckbox:true,category: 'Платформа для Программы лояльности:', showwordprefix:'', comment:'', price: '', stocked: false, name: ' - нотификация участников о результатах (e-mail)', arrOfVlue:'none'},
+	 {unicname:'disr',indexid:25,usehiddencheckbox:true,category: 'Платформа для Программы лояльности:', showwordprefix:'', comment:'', price: '', stocked: false, name: ' - функционал регистрации участников', arrOfVlue:'none'},
+		 
+	 //ЧЕТВЕРТЫЙ раздел
+	  {unicname:'disr',indexid:31,usehiddencheckbox:false,category: 'Дизайн key visual: ', showwordprefix:'', comment:'дизайн для платформы', price: '1500', stocked: true, name: 'Дизайн для платформы (website)', arrOfVlue:'none'},
+	  {unicname:'disr',indexid:32,usehiddencheckbox:false,category: 'Дизайн key visual: ', showwordprefix:'', comment:'Дизайн рекламного макета Программы', price: '150', stocked: true, name: 'Дизайн рекламного макета Программы', arrOfVlue:'none'},
+	  {unicname:'price_for_banner',indexid:33,usehiddencheckbox:false,category: 'Дизайн key visual: ', showwordprefix:'', comment:'дизайн баннеров для дистрибьюторов', price: '1800', stocked: true, name: 'Дизайн баннеров для дистрибьюторов', arrOfVlue:'none'},
+	  {unicname:'count_for_banner',indexid:34,usehiddencheckbox:true,category: 'Дизайн key visual: ', showwordprefix:'', comment:'some comment text', price: '', stocked: true, name: 'Количество баннеров', arrOfVlue:'inputbanner'},
+	  {unicname:'disr',indexid:35,usehiddencheckbox:false,category: 'Дизайн key visual: ', showwordprefix:'', comment:'some comment text', price: '1500', stocked: true, name: 'Дизайн рекламного макета Программы', arrOfVlue:'none'},
+	  {unicname:'disr',indexid:36,usehiddencheckbox:false,category: 'Дизайн key visual: ', showwordprefix:'', comment:'дизайн сертификатов победителям', price: '200', stocked: true, name: 'Дизайн сертификатов победителям (до 3 макетов)', arrOfVlue:'none'},  
+		  
+	  {unicname:'bonus_for_disr',indexid:16,usehiddencheckbox:false,category: 'Призовой фонд', showwordprefix:'от', comment:'Рассчет осуществляется на основе количества дистрибъюторов указаного во Вводном разделе', price: '300', stocked: true, name: 'Поощрения сотрудников дистрибьюторов (супервайзеров)', arrOfVlue:'none'}
 		];
 		var buttonListElements = [];
 		var lastCategory = null;
@@ -160,16 +202,23 @@ class DankButton extends React.Component {
 				//UNIC FORMULA
 				var usersControlFormula = [];
 				if (product.arrOfVlue=='inputdistributor') {
-					usersControlFormula.push(<div><input type="text"  onChange={this.setDistributorQuantiy.bind(this,index)}  value={this.state.value}/></div>);
+					usersControlFormula.push(<div><input className="width20" type="text"  onChange={this.setDistributorQuantiy.bind(this,index)}  value={this.state.value}/></div>);
+				} else if (product.arrOfVlue=='inputbanner') {
+					usersControlFormula.push(<div><input className="width20" type="text"  onChange={this.setBannerQuantiy.bind(this,index)}  value={this.state.value}/></div>);
+				} else if (product.arrOfVlue=='none') {
+					usersControlFormula.push(<div></div>);//<!-- for comment and so on-->
 				} else {		
 					usersControlFormula.push(<ProductSelect onClick={this.changeItemPriceMulti.bind(this,index)} category={product.arrOfVlue} key={product.indexid} />);
 				}
 				
 				//USE OR NOT checkbox
 				var useOrNotCheckbox = []
-				if (product.usecheckbox) {
-					useOrNotCheckbox
+				if (product.usehiddencheckbox) {
+					useOrNotCheckbox.push(<input className='checkboxforprice hiddencheckbox' type='checkbox' id={product.indexid}  onClick={this.handleClickCheckbox.bind(null, product.price,index)} value={product.price} checked/>);
+				} else {
+					useOrNotCheckbox.push(<input className='checkboxforprice' type='checkbox' id={product.indexid}  onClick={this.handleClickCheckbox.bind(null, product.price,index)} value={product.price}  />);
 				}
+				
 				  if (product.category !== lastCategory) {
 					buttonListElements.push(<div className="row bcwhite"><div className="col-md-12"><h3>{product.category}</h3></div></div>);
 				 }
@@ -183,7 +232,7 @@ class DankButton extends React.Component {
 				  buttonListElements.push(
 				  <div  key={product.indexid} className="row" title={product.comment}>
 					<div className="col-md-5 bcgray">
-						<input className='checkboxforprice' type='checkbox' id={product.indexid}  onClick={this.handleClickCheckbox.bind(null, product.price,index)} value={product.price} />
+				{useOrNotCheckbox}
 						<label   htmlFor={product.indexid}>{product.name}</label>
 					</div>
 					
@@ -208,10 +257,36 @@ class DankButton extends React.Component {
 	// render  
 	///////////
 	
+	setBannerQuantiy(indexinarray,event){
+		 /* var valueForBannerQuantity = event.target.value;
+		 var newArrForFormula = this.state.resultPrices;
+		 newArrForFormula[indexinarray].multinumber = valueForBannerQuantity;
+	 
+		 newArrForFormula.map(function(currentRow,index) {
+			   if (currentRow.unicname == 'price_for_banner') {
+				   newArrForFormula[index].multinumber = valueForBannerQuantity;
+			   }
+		   });   
+		  this.setState({ resultPrices: newArrForFormula });
+
+		  this.recountIt(); */
+	}
+	
 	setDistributorQuantiy(indexinarray,event){
 		  var valueForDistributorPrice = event.target.value;
 		  var newArrForFormula = this.state.resultPrices;
+		   newArrForFormula[indexinarray].multinumber = valueForDistributorPrice;
+		   //alert(newArrForFormula[indexinarray].multinumber);//количество дистрибъюторов
+		   
+	   newArrForFormula.map(function(currentRow,index) {
+			   if (currentRow.unicname == 'bonus_for_disr') {
+				   //alert('about change');
+				   newArrForFormula[index].multinumber = valueForDistributorPrice;
+			   }
+		   });
+		  this.setState({ resultPrices: newArrForFormula });
 		  
+		  this.recountIt();
 	}
 		/***
 		set new value for multinumber in state array (resultPrices) by indexinarray
@@ -254,15 +329,22 @@ class DankButton extends React.Component {
 			this.recountIt();
 	} 
 	/***
-	set new value for state prices (resultPrices)
+	set new AMOUNT value on state prices recalculation (resultPrices)
 	*/
 	recountIt() {
 		console.log('set it at last');
 		var forAmount = 0;
-	this.state.resultPrices.map(function(currentRow,index) {
-		//if(currentRow.price != 0 && currentRow.needit == true){
-		if(currentRow.needit == true){
-			forAmount = (parseFloat(forAmount) + parseFloat(currentRow.baseprice)*parseFloat(currentRow.multinumber)).toFixed(2);
+		this.state.resultPrices.map(function(currentRow,index) {
+		//only Price-generation stirngs must be used here
+		if (currentRow.baseprice != 0 && currentRow.needit == true ){
+			/*	if (currentRow.unicname == price_for_banner) {
+				alert('we in price!');
+			}
+			 	//forAmount = 600;// + (300*(parseInt(currentRow.multinumber)-1));			
+					forAmount =  parseFloat(forAmount) + (600 + (300*(parseInt(currentRow.multinumber)-1)));			
+			} else { 
+			}  */
+					forAmount = (parseFloat(forAmount) + parseFloat(currentRow.baseprice)*parseFloat(currentRow.multinumber)).toFixed(2);
 		}
 	});
 		
